324be6129a4a16158c7d960cc044a2c7a15df7c7
eb110 <wfigura@op.pl>
2020-03-17 11:01:22 +0000

    partials populated over the pages

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-
 const express = require('express')
 const app = express()
 
@@ -9,8 +8,6 @@ const passport = require('passport')
 //import index.js
 const routes = require('./routes/index');
 
-//layout
-const expressLayouts = require('express-ejs-layouts')
 const path = require('path');
 
 //db
@@ -44,28 +41,14 @@ app.use(passport.session())
 //reg/login typed by user data handling (sending to post method)
 app.use(express.urlencoded({ extended: false}))
 
-//body parser
-const bodyParser = require('body-parser');
-
-
-
 //view engine
 app.set('view engine', 'ejs');
-
-//hookup layouts
-app.set('layout', 'layouts/layout')
-app.use(expressLayouts)
 
 //HTMl -> hbs files (layouts)
 app.set('views', path.join(__dirname, 'views'));
 
 //Static folder
 app.use(express.static(path.join(__dirname, 'public')));
-
-//body parser - it has to be initiated before routes
-//it helps to read data send by forms
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true}));
 
 //routes
 app.use('/', routes);
